a02100b6e4ee27f7300c721be32c539b69a91f93
derekb123 <derekbutvin@gmail.com>
2020-08-04 03:57:12 +0000

    stored short/long url in database, redirected to new page. redirected to longurl page.

--- express_server.js
+++ express_server.js
@@ -51,7 +51,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  const sURL = generateRandomString(); // Log the POST request body to the console
+  urlDatabase[sURL] = req.body.longURL;
+  res.redirect(`/urls/${sURL}`);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
