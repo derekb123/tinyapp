29afa73598adc3aa353418982a6c419e58807f3c
derekb123 <derekbutvin@gmail.com>
2020-08-04 01:02:31 +0000

    set up urls file, created the endpoint in server file and connected it

--- express_server.js
+++ express_server.js
@@ -29,3 +29,11 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
