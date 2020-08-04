27ede5c7a2089057e228395a96709bf6e1c60bf1
derekb123 <derekbutvin@gmail.com>
2020-08-04 03:15:15 +0000

    updated so that server will read form post and give back some info

--- express_server.js
+++ express_server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+  let ranStr = Math.random().toString(36).slice(2, 8);
+  return ranStr;
 };
 
 app.get("/", (req, res) => {
@@ -30,6 +38,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
     shortURL: req.params.shortURL, 
@@ -37,3 +49,9 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
